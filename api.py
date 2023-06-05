503e172d9696dc28fbdd3fd4f2c7a4ac3f16c86f
sakkyoi <22865542+sakkyoi@users.noreply.github.com>
2023-06-06 00:35:37 +0800

    init

---
+++ api.py
@@ -0,0 +1,61 @@
+from fastapi import FastAPI, File, UploadFile
+import uvicorn
+
+from PIL import Image
+from io import BytesIO
+import base64
+import numpy as np
+import h5py
+
+import tensorflow as tf
+
+from model import VGGNet
+
+def query(image: BytesIO):
+    query_img = tf.keras.utils.load_img(image, target_size=(model.input_shape[0], model.input_shape[1]))
+    query_img = tf.keras.utils.img_to_array(query_img).astype(int)
+
+    query_feature = model.extract_feature(query_img, verbose=0)
+    scores = np.dot(query_feature, features.T)
+    rank_ID = np.argsort(scores)[::-1]
+    rank_score = scores[rank_ID]
+
+    imlist = [image_ids.astype(str)[index] for i, index in enumerate(rank_ID[0:10])]
+
+    results = []
+    for i, im in enumerate(imlist):
+        buffered = BytesIO()
+        Image.open(im).save(buffered, format="JPEG")
+        img_str = base64.b64encode(buffered.getvalue())
+        img_str = 'data:image/jpeg;base64,' + img_str.decode('utf-8')
+
+        results.append(img_str)
+
+    return results, rank_score[0:10].tolist()
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.post("/search")
+async def search(file: UploadFile = File(...)):
+    imlist, rank_score = query(BytesIO(file.file.read()))
+    return {"images": imlist, "scores": rank_score}
+
+@app.post("/search_kmeans")
+async def search_kmeans(file: UploadFile = File(...)):
+    imlist, rank_score = query(BytesIO(file.file.read()))
+    return {"images": imlist, "scores": rank_score}
+
+if __name__ == '__main__':
+    model = VGGNet()
+
+    # Load dataset
+    datasets = h5py.File('features.h5', 'r')
+    features = datasets['features'][:]
+    image_ids = datasets['image_ids'][:]
+    datasets.close()
+
+    uvicorn.run(app, host='0.0.0.0', port=8000)
---
+++ model.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+
+import numpy as np
+from numpy import linalg as LA
+
+from keras.applications.vgg16 import VGG16
+from keras.preprocessing import image
+from keras.applications.vgg16 import preprocess_input
+
+class VGGNet:
+    def __init__(self):
+        self.input_shape = (224, 224, 3)
+        self.weight = 'imagenet'
+        self.pooling = 'max'
+        self.model = VGG16(weights=self.weight, input_shape=(self.input_shape[0], self.input_shape[1], self.input_shape[2]), pooling=self.pooling, include_top=False)
+        self.model.predict(np.zeros((1, 224, 224, 3)))
+
+    def extract_feature(self, img_path: any, verbose='auto'):
+        if type(img_path) == str or type(img_path) == np.str_:
+            img = tf.keras.utils.load_img(img_path, target_size=(self.input_shape[0], self.input_shape[1]))
+            img = tf.keras.utils.img_to_array(img)
+            img = np.expand_dims(img, axis=0)
+            img = preprocess_input(img)
+            feature = self.model.predict(img, verbose=verbose)
+            feature_normalized = feature[0] / LA.norm(feature[0])
+        elif type(img_path) == np.ndarray:
+            img = img_path
+            img = np.expand_dims(img, axis=0)
+            img = preprocess_input(img)
+            feature = self.model.predict(img, verbose=verbose)
+            feature_normalized = feature[0] / LA.norm(feature[0])
+        else:
+            raise TypeError('img_path must be str or np.ndarray')
+        
+        return feature_normalized
